2bf63379f1195e9cc4afe8b3c37f5748058252a2
Mario Luja <mluja1@asu.edu>
2022-08-10 18:33:32 -0700

    Mobile menu fully functional, and added links to socials.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import {useRef} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TypeAnimation from 'react-type-animation';
 import AboutMe from './AboutMe';
@@ -7,9 +6,23 @@ import Projects from './Projects'
 import Skills from './Skills'
 import {GiHamburgerMenu} from 'react-icons/gi';
 import {Link} from 'react-scroll';
-
+import {SiGithub, SiLinkedin, SiLeetcode}  from 'react-icons/si';
 function App()
 {
+  const [shown,set_shown] = useState(false);
+
+  const open_mobile = () =>
+  {
+    if(shown === true)
+    {
+      set_shown(false);
+    }
+    else
+    {
+      set_shown(true);
+    }
+  }
+
   return(
     <div className="wrapper">
       <div className="def_wrapper">
@@ -19,9 +32,18 @@ function App()
               <li className="nav_bttn" ><Link to='about_section' smooth={true}>About Me</Link></li>
               <li className="nav_bttn"><Link to="project_section" smooth={true}>Projects</Link></li>
               <li className="nav_bttn"><Link to="skill_section" smooth={true}>Skills</Link></li>
-            <GiHamburgerMenu className="mobile_menu" size={25}/>
+            <GiHamburgerMenu className="mobile_icon" id="hamburger" onClick={open_mobile} size={25}/>
           </ul>
+          { shown && (
+          <div className="mobile_menu">
+              <div className="mobile_item"><Link to='about_section' smooth={true}>About Me</Link></div>
+              <div className="mobile_item"><Link to="project_section" smooth={true}>Projects</Link></div>
+              <div className="mobile_item"><Link to="skill_section" smooth={true}>Skills</Link></div>
+          </div>
+          )
+        }
         </nav>
+        
         <div className="def_contain">
           <div className="name_div">
             <TypeAnimation
@@ -42,21 +64,34 @@ function App()
               and is always ready to learn something new.
             </p>
           </div>
-          <button className="button" id="download"><b>Download Resume</b></button>
+          <div className="socials_div">
+            <ul className="socials_list">
+              <li className="social">
+                <a href="https://www.linkedin.com/in/mario-luja-452308200/" target="_blank">
+                  <SiLinkedin size={25} className="social_logo"/>
+                </a>
+              </li>
+              <li className="social">
+                <a href="https://github.com/mar10930" target="_blank">
+                  <SiGithub size={25} className="social_logo"/>
+                </a>
+              </li>
+              <li className="social">
+                <a href="https://leetcode.com/mario4education/" target="_blank">
+                  <SiLeetcode size={25} className="social_logo"/>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <AboutMe/>
       <Projects/>
       <Skills/>
     </div>
-
-    
   );
 
-
 }
-
-
 
 
 export default App;
