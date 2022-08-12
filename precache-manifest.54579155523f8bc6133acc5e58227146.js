647440a9b0caf31f7ee02507ecc9cdb2c223cf0b
Mario Luja <mluja1@asu.edu>
2022-08-12 00:02:18 -0700

    Updates

---
+++ precache-manifest.54579155523f8bc6133acc5e58227146.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0a23622b4fda0ce07d3",
+    "url": "/Online-Portfolio/static/css/main.9d217972.chunk.css"
+  },
+  {
+    "revision": "c0a23622b4fda0ce07d3",
+    "url": "/Online-Portfolio/static/js/main.c0a23622.chunk.js"
+  },
+  {
+    "revision": "325ca37fb911515c36cd",
+    "url": "/Online-Portfolio/static/js/1.325ca37f.chunk.js"
+  },
+  {
+    "revision": "e5a54710c91ec791b41a",
+    "url": "/Online-Portfolio/static/js/2.e5a54710.chunk.js"
+  },
+  {
+    "revision": "639f51babb90da111e0e",
+    "url": "/Online-Portfolio/static/js/runtime~main.639f51ba.js"
+  },
+  {
+    "revision": "7557293af20778265d30768f6b81df57",
+    "url": "/Online-Portfolio/static/media/ASU-logo.7557293a.png"
+  },
+  {
+    "revision": "2047ea8f1553dc2ec6032d5f358c3dea",
+    "url": "/Online-Portfolio/static/media/pokedex_site.2047ea8f.png"
+  },
+  {
+    "revision": "89ca319e67a7aa9fa09f3e40bf5afd62",
+    "url": "/Online-Portfolio/static/media/Tictactoe_app.89ca319e.PNG"
+  },
+  {
+    "revision": "0014e4d8a24665beec8cf69ccf7cf579",
+    "url": "/Online-Portfolio/index.html"
+  }
+];
